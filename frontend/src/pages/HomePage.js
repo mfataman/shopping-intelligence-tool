c5404a6dc5111a458a95470f6b24f21ec880fcd3
mfataman <ataman.ferhat@gmail.com>
2026-03-28 20:07:46 -0400

    Create HomePage component for the home page

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Shopping Intelligence Tool</h1>
+            <p>Your one-stop solution for shopping insights!</p>
+        </div>
+    );
+};
+
+export default HomePage;
